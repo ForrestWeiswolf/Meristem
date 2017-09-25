8e578a0a4331bcc33741ce9faa4c7e4e77e86a4d
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2017-09-25 15:20:58 -0400

    Start on format constructor

--- Format.js
+++ Format.js
@@ -1,4 +1,11 @@
 function Format(formatString, definitions) {
+  if (typeof formatString !== 'string'){
+    throw new Error('Incorrect first argument to Format constructor')
+  }
+  if (typeof definitions !== 'object'){
+    throw new Error('Incorrect second argument to Format constructor')
+  }
+
   this.formatString = formatString
   this.definitions = definitions
 }
