de827f8d76c1340f08cedf8f69661a5afc30ceaa
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2017-10-08 11:13:47 -0400

    Fixes to spec for recursive expansion

--- tests/FormatSpec.js
+++ tests/FormatSpec.js
@@ -5,7 +5,7 @@ describe('Format', () => {
     })
 
     it('Takes a string and an option non-empty object as arguments', () => {
-      let format = new Format('(a)', {a: 'example'})
+      let format = new Format('(a)', { a: 'example' })
       expect(format).toBeDefined()
       format = new Format('(a)')
       expect(format).toBeDefined()
@@ -17,11 +17,11 @@ describe('Format', () => {
     })
 
     it('Throws an error if first arg is not a string', () => {
-      function emptyConstructor(){
+      function emptyConstructor() {
         return new Format()
       }
 
-      function wrongTypeConstructor(){
+      function wrongTypeConstructor() {
         return new Format(1)
       }
 
@@ -38,40 +38,47 @@ describe('Format', () => {
     })
 
     it('returns a string', () => {
-      format = new Format('(A)', {'a': 'example'})      
+      format = new Format('(A)', { 'a': 'example' })
       expect(typeof format.expand()).toEqual('string')
     })
 
     it('when the string contains no parentheses, returns it as-is', () => {
-      format = new Format('ABC', {'a': 'example'})
+      format = new Format('ABC', { 'a': 'example' })
       expect(format.expand()).toEqual('ABC')
     })
 
     it('inserts strings from the definitions object in place of parenthetical tokens', () => {
-      format = new Format('(a), (b)(c)', {'a': 'foo', 'b': 'bar', 'c': '...'})
+      format = new Format('(a), (b)(c)', { 'a': 'foo', 'b': 'bar', 'c': '...' })
       expect(format.expand()).toEqual('foo, bar...')
     })
 
-    it('when the definition is an object with an expand method, it is called and the returned value added to result', () => {
-      format = new Format('(a), (b)(c)', {'a': 'foo', 'b': 'bar', 'c': '...'})
-      let recFormat = new Format('Recursive example: (recurse)', format)
-      expect(recFormat.expand()).toEqual('foo, bar...')
-    })
+    describe('when a token is itself a Format', () => {
+      it('uses passed definitions object if this.definitions does not exist', () => {
+        format = new Format('(a), (b)(c)')
+        expect(format.expand({ 'a': 'foo', 'b': 'bar', 'c': '...' })).toEqual('foo, bar...')
+      })
 
-    it('uses passed definitons object if this.definitions does not exist', () => {
-      format = new Format('(a), (b)(c)')
-      expect(format.expand()).toEqual({'a': 'foo', 'b': 'bar', 'c': '...'})
-    })
+      it('throws an error when this.definitions does not exist and no object is passed', () => {
+        format = new Format('(a)BC')
+        expect(format.expand).toThrow(new Error('This.definitions does not exist and no definitions argument passed'));
+      })
 
-    it('throws an error when this.definitions does not exist and no object is passed', () => {
-      format = new Format('(a)BC')
-      expect(format.expand).toThrow(new Error('This.definitions does not exist and no definitions argument passed'));      
-    })
+      it('when the definition is an object with an expand method, it is called and the returned value added to result', () => {
+        format = new Format('(a), (b)', { 'a': 'foo', 'b': 'bar' })
+        let recFormat = new Format('Recursive example: (recurse)', { 'recurse': format })
+        expect(recFormat.expand()).toEqual('Recursive example: foo, bar')
+      })
 
-    it('throws an error when a token is not found in the definitions object', () => {
-      format = new Format('(a)BC', {'d': 'example'})
-      expect(format.expand).toThrow(new Error('"a" not found in definitions'));      
-    })
+      it('passes the expand method this.definitions', () => {
+        format = new Format('(a), (b)')
+        let recFormat = new Format('Recursive example: (recurse)', { 'recurse': format, 'a': 'foo', 'b': 'bar' })
+        expect(recFormat.expand()).toEqual('Recursive example: foo, bar')
+      })
 
+      it('throws an error when a token is not found in the definitions object', () => {
+        format = new Format('(a)BC', { 'd': 'example' })
+        expect(format.expand).toThrow(new Error('"a" not found in definitions'));
+      })
+    }) //end 'when a token is itself a Format'
   })
 })
