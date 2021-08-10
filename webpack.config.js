5db1bf9150c76b3fa9ee8b09ddde129a45b2e000
Forrest Wolf <forrest.weiswolf@gmail.com>
2021-08-10 08:11:19 -0400

    Install and configure babel

--- webpack.config.js
+++ webpack.config.js
@@ -8,4 +8,16 @@ module.exports = {
     library: 'meristem',
     libraryTarget: 'umd',
   },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: { presets: ['@babel/preset-env'] },
+        },
+      },
+    ],
+  },
 }
