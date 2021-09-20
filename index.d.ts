a4fabbedf6a90fb5bec5355aa39a9188d4831249
Forrest Wolf <forrest.weiswolf@gmail.com>
2021-09-20 12:04:52 -0400

    Create first draft of WeightedRandom type declaration

---
+++ index.d.ts
@@ -0,0 +1,13 @@
+type WeightedOption = {
+  0: any,
+  1: number
+}
+
+declare module 'meristem' {
+  export class WeightedRandom {
+    constructor(options: {});
+    constructor(...options: Array<WeightedOption>);
+
+    choose: () => any;
+  }
+}
